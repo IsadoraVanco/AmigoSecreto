c92bbd303775ac7d57ec442a290ef41083e31812
Isadora <isadoravanco@gmail.com>
2025-03-13 20:34:29 -0300

    feat: :sparkles: Função para adicionar amigo

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+// ***********************************
+
+function adicionarAmigo() {
+    let campoNome = document.getElementById('amigo');
+    let nome = campoNome.value;
+    
+    console.log(`Amigo '${nome}'`);
+
+    if(nome == ''){
+        alert("Por favor, insira um nome");
+    }else{
+        amigos.push(nome);
+        campoNome.value = "";
+    }
+}
