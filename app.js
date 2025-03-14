adb7f2444f2f67760f6cd25a063d358096a3c1c8
Isadora <isadoravanco@gmail.com>
2025-03-13 21:47:01 -0300

    docs: :bulb: Comentários que descrevem as funções

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-let amigos = [];
-
-// ********** FUNÇÕES ************************************
-
+/**
+ * @brief Adiciona um amigo na lista
+ */
 function adicionarAmigo() {
     let campoNome = document.getElementById('amigo');
     let nome = campoNome.value;
     
     console.log(`Amigo '${nome}'`);
-
+    
     if(nome == ''){
         alert("Por favor, insira um nome");
     }else{
         amigos.push(nome);
         campoNome.value = "";
-
+        
         atualizarLista();
     }
 }
 
+/**
+ * @brief Atualiza a lista visível de amigos que serão sorteados
+ */
 function atualizarLista() {
     let campoLista = document.getElementById('listaAmigos');
     
@@ -25,24 +27,31 @@ function atualizarLista() {
     
     let elementosLista = "";
     let contador = 0;
-
+    
     while(contador < amigos.length){
         elementosLista += `<li>${amigos[contador]}</li>`;
         contador++;
     }
-
+    
     campoLista.innerHTML = elementosLista;
 }
 
+/**
+ * @brief Sorteia um amigo que está na lista
+ */
 function sortearAmigo() {
     if(amigos == ""){
         return;
     }
-
+    
     let indice = Math.floor(Math.random() * amigos.length);
     let nomeSorteado = amigos[indice];
-
+    
     let campoResultado = document.getElementById('resultado');
     
     campoResultado.innerHTML = nomeSorteado;
 }
+
+// ***********************************************************
+
+let amigos = [];
